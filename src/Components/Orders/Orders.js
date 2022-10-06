7f5ae39ad1da5d247022ed1d5926f057e680a1fe
arifjahan88 <arifjahan88@gmail.com>
2022-10-07 04:38:18 +0600

    after order section done

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,14 +1,30 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { removeFromDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItems/ReviewItem";
 
 const Orders = () => {
   const { products, givenCart } = useLoaderData();
   const [cart, setcart] = useState(givenCart);
+
+  const handleRemoveItem = (id) => {
+    const removeItem = cart.filter((product) => product.id !== id);
+    setcart(removeItem);
+    removeFromDb(id);
+  };
   return (
     <div>
       <div className="shop-container">
-        <div className="prooducts-container"></div>
+        <div>
+          {cart.map((product) => (
+            <ReviewItem
+              key={cart.id}
+              product={product}
+              handleRemoveItem={handleRemoveItem}
+            ></ReviewItem>
+          ))}
+        </div>
         <div className="details-container">
           <Cart cart={cart}></Cart>
         </div>
---
+++ src/Components/ReviewItems/ReviewItem.js
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import "./ReviewItem.css";
+
+const ReviewItem = ({ product, handleRemoveItem }) => {
+  const { id, img, name, price, quantity } = product;
+
+  return (
+    <div className="review-container">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div className="details-inside">
+        <div>
+          <h5>{name}</h5>
+          <p>
+            Price : $<span>{price}</span>
+          </p>
+          <p>
+            <small>
+              Quantity : <span>{quantity}</span>
+            </small>
+          </p>
+        </div>
+        <div>
+          <button onClick={() => handleRemoveItem(id)} className="button-class">
+            <FontAwesomeIcon className="icon-class" icon={faTrash} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewItem;
